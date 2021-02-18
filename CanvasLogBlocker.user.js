c986b9128ee965d804a090a1e3a20c9810c2ea60
drieda <65320245+drieda@users.noreply.github.com>
2021-02-18 08:23:47 +0100

    Update CanvasLogBlocker.user.js

--- CanvasLogBlocker.user.js
+++ CanvasLogBlocker.user.js
@@ -9,40 +9,41 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    $(document).ready(() =>{
-        let cssObj = {
-            position: 'fixed',
-            bottom: '0',
-            right: '0',
-            marginRight: '1%',
-            marginBottom: '0.5%',
-            fontSize: '1.5rem',
-            opacity: '50%',
-            zIndex: '1000',
-        }
-    
-        $("<div id='formFiller'>Action Log Blocker is Running on This Page<\div>").insertBefore("#wrapper #main").css(cssObj);
-        console.log('Using Action Log Blocker');
-    
-        /*
+    /*
         For not knowing whether some function reenables the event listeners or not
         the script attempts to remove them every 0.1s
         It is a bruteforce script anyway
-        */
-        setInterval(() => {
-            //Focus and blur are the 2 events canvas listens to
-            $(window).off('focus');
-            $(window).off('blur');
-    
-            //For safety reasons the script disables the focusout event listener too
-            $(window).off('focusout');
-    
-            console.log('listeners removed');
-        }, 100);
+    */
+   console.log('Using Action Log Blocker');
 
-    });
-    
+  setInterval(() => {
+    //Focus and blur are the 2 events canvas listens to
+    $(window).off('focus');
+    $(window).off('blur');
+
+    //For safety reasons the script disables the focusout event listener too
+    $(window).off('focusout');
+
+    console.log('listeners removed');
+  }, 100);
+
+  $(document).ready(() => {
+    let cssObj = {
+      position: 'fixed',
+      bottom: '0',
+      right: '0',
+      marginRight: '1%',
+      marginBottom: '0.5%',
+      fontSize: '1.5rem',
+      opacity: '50%',
+      zIndex: '1000',
+    };
+
+    $("<div id='formFiller'>Action Log Blocker is Running on This Page<div>")
+      .insertBefore('#wrapper #main')
+      .css(cssObj);
+  });
 })();
